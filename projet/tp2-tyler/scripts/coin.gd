4360b719c49b5828fb7357cf67211ed0d697effb
TTylerRRichard <2496174@cmontmorency.qc.ca>
2025-10-07 13:55:33 -0400

    recent

--- projet/tp2-tyler/scripts/coin.gd
+++ projet/tp2-tyler/scripts/coin.gd
@@ -1,6 +1,7 @@
 extends Area2D
 
+@onready var animation_player = $AnimationPlayer
 
 func _on_body_entered(body):
 	print("+! coin!")
-	queue_free()
+	animation_player.play("pickup")
--- projet/tp2-tyler/scripts/player.gd
+++ projet/tp2-tyler/scripts/player.gd
@@ -6,6 +6,9 @@ const JUMP_VELOCITY = -300.0
 
 @onready var animated_sprite = $AnimatedSprite2D
 
+@onready var jump: AudioStreamPlayer2D = $jump
+@onready var run: AudioStreamPlayer2D = $run
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
@@ -14,13 +17,15 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-
+		jump.play()
+		
 # get the input direction: -1, 0, 1
 	var direction := Input.get_axis("move_left", "move_right")
 	
 	#flips sprite
 	if direction > 0:
 		animated_sprite.flip_h = false
+		
 	elif direction < 0:
 		animated_sprite.flip_h = true
 		
@@ -38,6 +43,7 @@ func _physics_process(delta: float) -> void:
 			animated_sprite.play("idle")
 		else: animated_sprite.play("run")
 	#apply movement
+	
 	if direction:
 		velocity.x = direction * SPEED
 	else:
